b286e129474ee5aa879f0b397c8d1fc44c9ed351
ArnoMolenaers <60972933+ArnoMolenaers@users.noreply.github.com>
2020-06-14 10:54:01 +0200

    jupiter

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,8 @@
 
-
-import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
